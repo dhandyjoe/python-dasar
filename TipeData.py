ba065c5925d67dcbd61d10f3e1860483572361b7
dhandyjoe <dhanjoenkp@gmail.com>
2021-05-06 19:58:36 +0700

    menambahkan materi tipe data

---
+++ TipeData.py
@@ -0,0 +1,24 @@
+
+# String
+string1 = "Hello Python"
+
+# Integer
+int1 = 1
+
+# Boolean 
+bool1 = True # false
+
+# Float 
+float1 = 1.2
+
+# List -> Mutable ( indeks bisa diubah )
+list1 = ["sd", 1, 1.2]
+print(type(list1))
+
+# Tuple -> Immutable ( indeks tidak bisa diubah ) 
+tuple1 = (1, 3.2, "Dhandy")
+print(type(tuple1))
+
+# Dictionary -> menyimpan beberapa tipe data beserta value nya
+dictionary1 = {"nama" : "Dhandy", "id" : 2}
+print(type(dictionary1))
